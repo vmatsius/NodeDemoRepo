cd577c5250e1494b9cf63ccffc20e5e4c6401924
vmatsius <vmatsius@ucdavis.edu>
2021-03-17 12:25:47 -0700

    3

--- classPractice/1classPractice/importDate.js
+++
@@ -1,4 +0,0 @@
-var date=require('./date.js')
-
-console.log("date is: ", date.myDate())
-date.writeToConsole()
--- module2/appendFile.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs')
-
-fs.appendFile('xyz.txt','\n second line',function(err,data){
-    if(err)
-        console.error(err)
-    else
-        console.log('Appended!!')
-})
